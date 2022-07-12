2b4aaa2d15d36b0ed79c712299b98cbd9b62b410
rostyslav-shyian-exa <rostyslav.shyian@exabeam.com>
2022-07-12 14:14:21 +0300

    added initial props

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,18 @@
 import { AppProps } from "next/dist/next-server/lib/router/router";
+import Head from "../node_modules/next/head";
 
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>My top</title>
+        <link rel="stylesheet" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
 export default MyApp;
