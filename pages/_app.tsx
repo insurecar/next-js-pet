385435546782052c190102b4c077936b19fd573d
rostyslav-shyian-exa <rostyslav.shyian@exabeam.com>
2022-07-13 13:54:43 +0300

    added typography

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,6 +9,12 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
       <Head>
         <title>My top</title>
         <link rel="stylesheet" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,400;0,500;1,100&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <Component {...pageProps} />;
     </>
