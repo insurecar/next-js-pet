c84420c889393a5312197359dc7804a568ff58a4
rostyslav-shyian-exa <rostyslav.shyian@exabeam.com>
2022-09-21 12:36:43 +0300

    added configure for svg

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,22 +1,29 @@
 import styles from "./Button.module.css";
 import { ButtonProps } from "./Button.props";
 import cn from "classnames";
+import ArrowIcon from './arrow.svg';
 
 export const Button = ({
   appearence = "primary",
+  arrow = 'none',
   children,
   className,
   ...props
 }: ButtonProps): JSX.Element => {
   return (
     <button
-      className={cn(styles.button, {
+      className={cn(styles.button, className, {
         [styles.primary]: appearence === "primary",
         [styles.ghost]: appearence === "ghost",
       })}
       {...props}
     >
       {children}
+      {arrow !== 'none' && <span className={cn(styles.arrow, {
+        [styles.down]: arrow === 'down'
+      })}>
+        <ArrowIcon />
+      </span>}
     </button>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,11 @@
-import { Htag } from "../components";
-import { Button } from "../components/Button/Button";
+import { Htag, Button } from "../components";
 
 export default function Home(): JSX.Element {
   return (
     <>
       <Htag tag="h1">Текст</Htag>
-      <Button appearence="primary">Qwertu</Button>
-      <Button appearence="ghost">Qwertu</Button>
+      <Button appearence="primary" arrow='down'>Qwertudihjuthg</Button>
+      <Button appearence="ghost" arrow='right'>Qwertu</Button>
     </>
   );
 }
