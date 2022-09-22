6912c8d4ed946d30f0f5f5a56853a3e9a7f4fdd7
Rostyslav Shyian <51438396+insurecar@users.noreply.github.com>
2022-09-22 13:43:40 +0300

    Future css grid (#2)
    * added styles to wrapper
    
    * added styles to footer
    
    Co-authored-by: rostyslav-shyian-exa <rostyslav.shyian@exabeam.com>

--- Layout/Footer/Footer.tsx
+++ Layout/Footer/Footer.tsx
@@ -1,8 +1,12 @@
 import { FooterProps } from "./Footer.props";
-import styles from './Header.module.css';
+import styles from './Footer.module.css';
+import cn from 'classnames';
+import { format } from 'date-fns';
 
-export const Footer = ({ ...props }: FooterProps): JSX.Element =>
-    <div {...props}>
-        Footer
+export const Footer = ({ className, ...props }: FooterProps): JSX.Element =>
+    <div className={cn(className, styles.footer)} {...props}>
+        <div>OwlTop © 2020 - {format(new Date(), 'yyyy')} Всі права захищені</div>
+        <a href="#" target='_blank'>Користувацькі угоди</a>
+        <a href="#" target='_blank'>Політика конфіденційності</a>
     </div>;
 
--- Layout/Layout.tsx
+++ Layout/Layout.tsx
@@ -2,18 +2,17 @@ import { LayoutProps } from "./Layout.props";
 import styles from './Layout.module.css';
 
 import { Header, Sidebar, Footer } from "./indexLayoutExport";
-import { Component, FunctionComponent } from "react";
+import { FunctionComponent } from "react";
+
 export const Layout = ({ children }: LayoutProps): JSX.Element =>
-    <>
-        <Header />
-        <div>
-            <Sidebar />
-            <div>
-                {children}
-            </div>
+    <div className={styles.wrapper}>
+        <Header className={styles.header} />
+        <Sidebar className={styles.sidebar} />
+        <div className={styles.body}>
+            {children}
         </div>
-        <Footer />
-    </>;
+        <Footer className={styles.footer} />
+    </div>;
 
 export const withLayout = <T extends Record<string, unknown>>(Component: FunctionComponent<T>) => {
     return function withLayoutComponent(props: T): JSX.Element {
