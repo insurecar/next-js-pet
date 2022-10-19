862a0d405b08a28d1ac513a32db7f35b5a777d39
Rostyslav Shyian <51438396+insurecar@users.noreply.github.com>
2022-10-19 15:10:40 +0300

    added getSataticPaths
    added getSataticPaths

---
+++ pages/courses/[alias].tsx
@@ -0,0 +1,29 @@
+import { withLayout } from "../../Layout/Layout";
+import { GetStaticProps } from "next";
+import axios from "axios";
+import { MenuItem } from "../../interfaces/menu.interfaces";
+
+const Course = ({ menu, firstCategory }: CourseProps): JSX.Element => {
+  return <div>sdg</div>;
+};
+
+export default withLayout(Course);
+
+export const getStaticProps: GetStaticProps<CourseProps> = async () => {
+  const firstCategory = 0;
+  const { data: menu } = await axios.get<CourseProps[]>(
+    process.env.NEXT_PUBLIC_DOMAIN + "/category"
+  );
+
+  return {
+    props: {
+      menu,
+      firstCategory,
+    },
+  };
+};
+
+interface CourseProps extends Record<string, unknown> {
+  menu: MenuItem[];
+  firstCategory: number;
+}
