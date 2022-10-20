8fb03ac31560c1a4dd6685a30772f0bdf87de738
Rostyslav Shyian <51438396+insurecar@users.noreply.github.com>
2022-10-20 11:51:09 +0300

    sdf

--- pages/courses/[alias].tsx
+++ pages/courses/[alias].tsx
@@ -9,9 +9,9 @@ const Course = ({ menu, firstCategory }: CourseProps): JSX.Element => {
 
 export default withLayout(Course);
 
-export const getStaticProps: GetStaticProps<CourseProps> = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const firstCategory = 0;
-  const { data: menu } = await axios.get<CourseProps[]>(
+  const { data: menu } = await axios.get<MenuItem[]>(
     process.env.NEXT_PUBLIC_DOMAIN + "/category"
   );
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,50 +2,56 @@ import { useState } from "react";
 import { Htag, Button, Ptag, Tag, Rating } from "../components";
 import { withLayout } from "../Layout/Layout";
 import { GetStaticProps } from "next";
-import axios from 'axios';
+import axios from "axios";
 import { MenuItem } from "../interfaces/menu.interfaces";
-
 
 const Home = ({ menu, firstCategory }: HomeProps): JSX.Element => {
   const [r, setR] = useState<number>(3);
   console.log(menu);
 
-
-  console.log(process.env.NEXT_PUBLIC_DOMAIN)
+  console.log(process.env.NEXT_PUBLIC_DOMAIN);
   return (
     <>
       <Htag tag="h1">Текст</Htag>
-      <Button appearence="primary" arrow='down'  >Qwertudihjuthg</Button>
-      <Button appearence="ghost" arrow='right'>Qwertu</Button>
-      <Ptag size='l'>fiuhyu</Ptag>
-      <Tag color='red' size='m'>dfijgurgh</Tag>
-      <Tag color='primary' size='s' href='pravda.com.ua'>12sf</Tag>
-      <Tag color='green' size='m' href='pravda.com.ua'>12sf</Tag>
+      <Button appearence="primary" arrow="down">
+        Qwertudihjuthg
+      </Button>
+      <Button appearence="ghost" arrow="right">
+        Qwertu
+      </Button>
+      <Ptag size="l">fiuhyu</Ptag>
+      <Tag color="red" size="m">
+        dfijgurgh
+      </Tag>
+      <Tag color="primary" size="s" href="pravda.com.ua">
+        12sf
+      </Tag>
+      <Tag color="green" size="m" href="pravda.com.ua">
+        12sf
+      </Tag>
       <Rating rating={r} isEditable={true} setRating={setR} />
-      <ul>
-
-        {menu.map(m => <li key={m._id.secondCategory} >{m._id.secondCategory}</li>)}
-      </ul>
+      <ul></ul>
     </>
   );
 };
 
 export default withLayout(Home);
 
-export const getStaticProps: GetStaticProps = async () => {
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
   const firstCategory = 0;
-  const { data: menu } = await axios.get<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/category');
+  const { data: menu } = await axios.get<MenuItem[]>(
+    process.env.NEXT_PUBLIC_DOMAIN + "/category"
+  );
 
   return {
     props: {
       menu,
-      firstCategory
-    }
+      firstCategory,
+    },
   };
 };
 
 interface HomeProps extends Record<string, unknown> {
   menu: MenuItem[];
   firstCategory: number;
-
 }
